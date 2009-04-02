581871a143ed0baa8d793e1de615951a8d7b3d0b
Viktor Mauch <viktor@viktormauch.de>
2009-04-02 11:22:25 +0000

    Prepared for 2.0
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/trunk@111 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

--- scripts/getLogFiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-LOGFILES="logx  logxfromfzk  logxfromfzkDebug  logxprod  monjobs_happyface_summary.xml qstat.xml";
-
-for i in $LOGFILES; do
-    wget http://ekpganglia.physik.uni-karlsruhe.de/~happyface/logs/out/$i -O ../inputFiles/$i
-done
