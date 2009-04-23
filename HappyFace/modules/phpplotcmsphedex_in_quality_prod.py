3ef3fe75a4ea7b576128257f978c4f23c9eac9f0
Volker Buege <v.buege@enbw.com>
2009-04-23 15:31:31 +0000

    CMS Phedex Module and corresponding plots included
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/tags/v2.0-alpha@121 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

---
+++ HappyFace/modules/phpplotcmsphedex_in_quality_prod.py
@@ -0,0 +1,8 @@
+from PhpPlotCMSPhedex import *
+
+class phpplotcmsphedex_in_quality_prod(PhpPlotCMSPhedex):
+
+    def __init__(self,category,timestamp,storage_dir):
+
+        PhpPlotCMSPhedex.__init__(self,category,timestamp,storage_dir)
+
---
+++ HappyFace/modules/phpplotcmsphedex_out_quality_prod.py
@@ -0,0 +1,8 @@
+from PhpPlotCMSPhedex import *
+
+class phpplotcmsphedex_out_quality_prod(PhpPlotCMSPhedex):
+
+    def __init__(self,category,timestamp,storage_dir):
+
+        PhpPlotCMSPhedex.__init__(self,category,timestamp,storage_dir)
+
