307b84d7215bdbff126cd02af1a2d3493ad96b75
Viktor Mauch <viktor@viktormauch.de>
2009-04-24 15:29:27 +0000

    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/trunk@123 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

--- HappyFace/modules/cms_phedex_prod_from_quality_72h.py
+++
@@ -1,7 +0,0 @@
-from CMSPhedexPlot import *
-
-class cms_phedex_prod_from_quality_72h(CMSPhedexPlot):
-
-    def __init__(self,category,timestamp,storage_dir):
-
-        CMSPhedexPlot.__init__(self,category,timestamp,storage_dir)
