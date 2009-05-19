8e0a36570fa8a8c6591c42eb835d7b197b699f1b
Viktor Mauch <viktor@viktormauch.de>
2009-05-19 12:33:09 +0000

    Changed to validate against css v3.0
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/trunk@172 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

--- HappyFace/output/web/CategoryContentTab.py
+++ HappyFace/output/web/CategoryContentTab.py
@@ -12,7 +12,7 @@ class CategoryContentTab(object):
 	<a href="http://validator.w3.org/check?uri=referer"><img style="border:0;vertical-align: middle;" src="config/images/valid-xhtml11.png" alt="Valid XHTML 1.1" /></a>
 	"""
 	valid_css = """
-	<a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;vertical-align: middle;" src="config/images/vcss.gif" alt="Valid CSS!" /></a>
+	<a href="http://jigsaw.w3.org/css-validator/check/referer?&amp;profile=css3"><img style="border:0;vertical-align: middle;" src="config/images/vcss.gif" alt="Valid CSS!" /></a>
 	"""
 	python = """
 	<a href="http://python.org"><img style="border:0;vertical-align: middle;" src="config/images/python_logo_mini.png" alt="Python" /></a>
