ae691ed948d5fd30ae3396858a4c9aa53ee39fcd
Volker Buege <v.buege@enbw.com>
2009-07-23 15:35:36 +0000

    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/trunk@208 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

---
+++ HappyFace/modules/sam_cms.py
@@ -0,0 +1,7 @@
+from Sam import *
+
+class sam_cms(Sam):
+
+    def __init__(self,category,timestamp,storage_dir):
+
+        Sam.__init__(self,category,timestamp,storage_dir)
---
+++ HappyFace/modules/sam_ops.py
@@ -0,0 +1,7 @@
+from Sam import *
+
+class sam_ops(Sam):
+
+    def __init__(self,category,timestamp,storage_dir):
+
+        Sam.__init__(self,category,timestamp,storage_dir)
