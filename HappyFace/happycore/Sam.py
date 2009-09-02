ac389c2fd46e02342bff37615d0b6ee7b74e828c
Friederike Nowak <friederike.nowak@desy.de>
2009-09-02 11:50:11 +0000

    removed bug in status determination
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/trunk@219 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

--- HappyFace/happycore/Sam.py
+++ HappyFace/happycore/Sam.py
@@ -2,11 +2,6 @@ from ModuleBase import *
 from DownloadTag import *
 from XMLParsing import *
 
-#############################################
-# class to donwload plots (via WGET command)
-# self.url has to be defined by the inhereting module
-#############################################
-
 class Sam(ModuleBase):
 
     def __init__(self, category, timestamp, archive_dir):
@@ -146,8 +141,11 @@ class Sam(ModuleBase):
 	Type = ""
 	Time = ""
 
-	for services_item in services:
+        serviceStatusList = []
 
+        
+	for services_item in services:
+            
 	    for services_prop in services_item:
 		if services_prop.tag == "ServiceType":
 		    ServiceType = services_prop.text
@@ -156,6 +154,8 @@ class Sam(ModuleBase):
 		if services_prop.tag == "ServiceNames":
 		    sn = services_prop
 
+                    serviceTestStatusList = []
+                    
 		    for sn_item in sn:
 			
 		        for sn_prop in sn_item:
@@ -166,23 +166,31 @@ class Sam(ModuleBase):
 			    if sn_prop.tag == "ServiceStatus":
 			        ServiceStatus = sn_prop.text
 
+                        if ServiceStatus == 'ok': service_status = 1.
+                        elif ServiceStatus == 'warn': service_status = 0.5
+                        else : service_status = 1.
+                        
+                        details_db_values["service_type"] = ServiceType
+                        details_db_values["service_name"] = ServiceName
+                        details_db_values["service_status"] = service_status
+
 			for sn_prop in sn_item:
 			    if sn_prop.tag == "Tests":
 			        tests = sn_prop
 
 			        for tests_item in tests:
-
+                                    
 			            for tests_prop in tests_item:
 
 				        if tests_prop.tag == "Status": Status = tests_prop.text
-				        elif tests_prop.tag == "Url": Url = tests_prop.text
+                                        elif tests_prop.tag == "Url": Url = tests_prop.text
 				        elif tests_prop.tag == "Age": Age = tests_prop.text
 				        elif tests_prop.tag == "Type": Type = tests_prop.text
 				        elif tests_prop.tag == "Time": Time = tests_prop.text
-			
-				    details_db_values["service_type"] = ServiceType
-				    details_db_values["service_name"] = ServiceName
-				    details_db_values["service_status"] = ServiceStatus
+
+                                    testStatus = self.determineTestStatus(str(Status))
+                                    serviceTestStatusList.append(testStatus)
+                                    
 				    details_db_values["status"] = Status
 				    details_db_values["url"] = (self.report_url + Url.__str__()).replace('&','&amp;').replace('%','%%')
 				    details_db_values["age"] = Age
@@ -192,11 +200,18 @@ class Sam(ModuleBase):
 				    # store the values to the database
 				    Details_DB_Class(**details_db_values)
 
+                    #print ServiceName
+                    #print serviceTestStatusList
+                    serviceStatusGlobal = self.determineServiceStatus(serviceTestStatusList)
+                    serviceStatusList.append(serviceStatusGlobal)              
+
+
+
 	# unlock the database access
 	self.lock.release()
 
         # at the moment always happy
-        self.status = 1.0
+        self.status = self.determineStatus(serviceStatusList)
 
 
     def output(self):
@@ -214,8 +229,11 @@ class Sam(ModuleBase):
        	{
 	    if ($temp_element != $info["service_name"]) {
 
-		if ($info["service_status"] == "1") {
+		if ($info["service_status"] == "1.") {
 		    $service_status_color_flag = "success";
+		}
+                elseif ($info["service_status"] == "0.5") {
+		    $service_status_color_flag = "warning";
 		} else {
 		    $service_status_color_flag = "fail";
 		}
@@ -270,3 +288,42 @@ class Sam(ModuleBase):
 	"""
 
         return self.PHPOutput(module_content)
+
+
+
+    def determineStatus(self, serviceStatusList):
+        status = 1.
+
+        for serviceStatus in serviceStatusList:
+            if serviceStatus < status:
+                status = serviceStatus
+
+        if len(serviceStatusList) == 0:
+            status = -1.
+
+        return status
+    
+
+    def determineTestStatus(self,StatusString):
+        testStatus = 1.
+
+        if StatusString == 'ok':
+            testStatus = 1.
+        elif StatusString == 'warn':
+            testStatus = 0.5
+        elif StatusString == 'error':
+            testStatus = 0.
+
+        return testStatus
+
+    def determineServiceStatus(self,StatusList):
+        status = 1.
+
+        for testStatus in StatusList:
+            if testStatus < status:
+                status = testStatus
+
+        if len(StatusList) == 0:
+            status = -1.
+
+        return status
