94abec92845da555d8d5fb028d27b3cea756d33f
Volker Buege <v.buege@enbw.com>
2009-10-21 14:04:57 +0000

    Modification of Plot.py in order to support multiplots
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/trunk@272 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

--- HappyFace/happycore/Plot.py
+++ HappyFace/happycore/Plot.py
@@ -12,62 +12,78 @@ class Plot(ModuleBase):
     def __init__(self, category, timestamp, archive_dir):
         ModuleBase.__init__(self, category, timestamp, archive_dir)
 
-
-        # definition of the database table keys and pre-defined values
-	self.db_keys['url'] = StringCol()
-	self.db_keys['filename'] = StringCol()
-	
-	self.db_values['url'] = ""
-	self.db_values['filename'] = ""
-
-        self.plotTag = 'plot'
-
+        self.plots = {}
         
+
                 
     def run(self):
 
         # run the test
 
-        if not self.plotTag in self.downloadRequest:
-            err = 'Error: Could not find required tag: '+self.plotTag+'\n'
+
+        for tag in self.downloadRequest.keys():
+            if tag.find('plot') == 0:
+                self.plots[tag] = tag.replace('plot','')
+                
+
+
+        if len(self.plots) == 0:
+            err = 'Error: Could not find download tag(s)\n'
             sys.stdout.write(err)
             self.error_message +=err
+
+        if self.error_message != "":
+            sys.stdout.write(self.error_message)
             return -1
 
+        plotsList =  self.plots.keys()
+        plotsList.sort()
+        for tag in plotsList:
+            
+            ident = self.plots[tag]
+
+            fileType = self.downloadService.getFileType(self.downloadRequest[tag])
+
+            self.configService.addToParameter('setup','source',tag+": "+self.downloadService.getUrlAsLink(self.downloadRequest[tag])+"<br>")
 
 
-        fileType = self.downloadService.getFileType(self.downloadRequest[self.plotTag])
-
-        self.configService.addToParameter('setup','source',self.downloadService.getUrlAsLink(self.downloadRequest[self.plotTag]))
-
-
-        filenameFullPath = self.archive_dir +"/" + self.__module__+fileType
-        success,stderr = self.downloadService.copyFile(self.downloadRequest[self.plotTag],filenameFullPath)
-        self.error_message +=stderr
+            filenameFullPath = self.archive_dir +"/" + self.__module__+ident+fileType
+            success,stderr = self.downloadService.copyFile(self.downloadRequest[tag],filenameFullPath)
+            self.error_message +=stderr
         
-	if success == True:
-	    self.status = 1.0
-	    filename = "archive/" + str(self.timestamp) + "/" + self.__module__+fileType
-	else:
-	    filename = ""
+            if success == True:
+                self.status = 1.0
+                filename = "archive/" + str(self.timestamp) + "/" + self.__module__+ident+fileType
+            else:
+                filename = ""
 
 
 	# definition fo the database table values
-        # self.db_values['url'] = self.url.replace('&','&amp;').replace('%','%%') # the replacement ensures the XHTML validation by W3C / printf() PHP command
+	    self.db_keys['filename'+ident] = StringCol()
+            self.db_values['filename'+ident] = filename
 
-	self.db_values['filename'] = filename
-	self.db_values['url'] = filename
         
     def output(self):
 
         # this module_content string will be executed by a printf('') PHP command
         # all information in the database are available via a $data["key"] call
-        module_content = """
-        <?php
-        printf('
-        <a href="' .$data["url"]. '"><img alt="" src="' .$data["filename"]. '" style="border: 0px solid;" /></a>
-        ');
-        ?>
-        """
+        mc = []
+        mc.append("<?php")
+        mc.append("printf('")
+        plotsList =  self.plots.keys()
+        plotsList.sort()
+        for tag in plotsList:
+            filename = 'filename'+self.plots[tag]
+#            url = 'url'+self.plots[tag]
+            mc.append("""
+            <a href="' .$data["""+filename+"""]. '"><img alt="" src="' .$data["""+filename+"""]. '" style="border: 0px solid;" /></a><br>
+            """)
+        mc.append("');")
+        mc.append(' ?>')
+            
+        module_content = ""
+        for i in mc:
+            module_content +=i+"\n"
+
         
         return self.PHPOutput(module_content)
--- HappyFace/happycore/SinglePlot.py
+++ HappyFace/happycore/SinglePlot.py
@@ -9,4 +9,15 @@ class SinglePlot(Plot):
 
         Plot.__init__(self, category, timestamp, archive_dir)
 	
+        self.singlePlotTag = 'plot'
+        configDownloadRequests = self.configService.getDownloadRequests()
+        if not configDownloadRequests.has_key('plot'):
+            err = 'Error in '+ self.__module__+': Could not find download tag plot.\n'
+            sys.stdout.write(err)
+            self.error_message +=err
+
+        elif len(configDownloadRequests) > 1:
+            err = 'Error '+ self.__module__+': Found more than 1 download for SinglePlot.\n'
+            sys.stdout.write(err)
+            self.error_message +=err
 
