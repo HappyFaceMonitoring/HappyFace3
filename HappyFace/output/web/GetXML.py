8cbdda57db5a806347f598a115480fac1b9a4f31
Armin Burgmeier <burgmeier@ekp.uni-karlsruhe.de>
2010-02-15 16:22:44 +0000

    Make sure category status in XML output matches the one in HTML output
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/trunk@353 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

--- HappyFace/output/web/GetXML.py
+++ HappyFace/output/web/GetXML.py
@@ -6,6 +6,7 @@ class GetXML(object):
 
 	declare_cat_titles = '$cat_titles = array(';
 	declare_cat_types = '$cat_types = array(';
+	declare_cat_algos = '$cat_algos = array(';
 	declare_cat_ids = '$cat_ids = array(';
 
 	cat_id = 0
@@ -13,21 +14,25 @@ class GetXML(object):
         for category in config.get('setup','categories').split(","):
 	    cat_title = config.get(category,'cat_title')
 	    cat_type  = config.get(category,'cat_type')
+	    cat_algo  = config.get(category,'cat_algo')
 
 	    if not first_iteration:
 	        declare_cat_titles += ", ";
 	        declare_cat_types += ", ";
+		declare_cat_algos += ", ";
 	        declare_cat_ids += ", ";
 	    else:
 	        first_iteration = False
 
 	    declare_cat_titles += "'" + category + "' => '" + cat_title + "'"
 	    declare_cat_types  += "'" + category + "' => '" + cat_type  + "'"
+	    declare_cat_algos  += "'" + category + "' => '" + cat_algo  + "'"
 	    declare_cat_ids    += "'" + category + "' => '" + str(cat_id) + "'"
 	    cat_id += 1
 
 	declare_cat_titles += ');'
 	declare_cat_types += ');'
+	declare_cat_algos += ');'
 	declare_cat_ids += ');'
 
 	web_title = config.get('setup','web_title')
@@ -52,10 +57,10 @@ class GetXML(object):
 	{
 	    $pageURL = curPageURL();
 
-	    """ + declare_cat_titles + "\n" + declare_cat_types + "\n" + declare_cat_ids + """
+	    """ + declare_cat_titles + "\n" + declare_cat_types + "\n" + declare_cat_ids + "\n" + declare_cat_algos + """
 
-            # Get different categories with their type from the result Matrix
-	    $cat_modules = array();
+    # Get different categories with their type from the result Matrix
+    $cat_modules = array();
 	    $cat_info = array();
 	    foreach ($myModuleResultsArray as $module=>$data) {
 	        $date = date('Y-m-d', $data['timestamp']);
@@ -66,7 +71,7 @@ class GetXML(object):
 	            $cat_modules[$data['category']] = '';
 	            $cat_info[$data['category']]['name'] = $data['category'];
 	            $cat_info[$data['category']]['title'] = $cat_titles[$data['category']];
-		    $cat_info[$data['category']]['status'] = $data['status'];
+		    $cat_info[$data['category']]['status'] = getCatStatus($data['category'], $cat_algos[$data['category']], $myModuleResultsArray);
 		    $cat_info[$data['category']]['type'] = $cat_types[$data['category']];
 		    $cat_info[$data['category']]['link'] = "$pageURL?date=$date&amp;time=$time&amp;t=$cat_id";
 		}
@@ -79,12 +84,6 @@ class GetXML(object):
 		$cat_modules[$data['category']] .= "   <time>$date $time</time>\n";
 		$cat_modules[$data['category']] .= "   <link>$pageURL?date=$date&amp;time=$time&amp;t=$cat_id&amp;m=$module</link>\n";
 		$cat_modules[$data['category']] .= "  </module>\n";
-
-	        if($data['status'] < $cat_info[$data['category']]['status'])
-	            $cat_info[$data['category']]['status'] = $data['status'];
-
-	        if($data['mode_type'] == 'rated' && $cat_info[$data['category']]['type'] != 'rated')
-	            $cat_info[$data['category']]['type'] = 'rated';
 	    }
 
             echo '<happyface>' . "\n";
