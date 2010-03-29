d09cea81a34cd58cb796598560de49341f4f5717
Armin Burgmeier <burgmeier@ekp.uni-karlsruhe.de>
2010-03-29 14:20:21 +0000

    Replace database locking by a lockfile
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/trunk@391 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

--- HappyFace/run.py
+++ HappyFace/run.py
@@ -69,148 +69,152 @@ def HappyFace():
         sys.stdout.write('Could not create archive directory ' + archive_dir + ', aborting ...\n')
         sys.exit(-1)
 
-    # try to initiate / create the database
-    database = output_dir + "/HappyFace.db"
+    # Check for lockfile existence
+    # TODO: Remove the race
+    lockfile = output_dir + "/hf.lock"
+    if os.path.exists(lockfile):
+        sys.stdout.write('Lock file exists. Is there another HappyFace instance running? Aborting ...\n')
+	sys.exit(-1)
+
     try:
-        # has to be the full path
-        connection_string = 'sqlite:' + os.getcwd() + "/" + database
-        connection = connectionForURI(connection_string)
+        open(lockfile, 'w').close()
 
-	# Acquire database lock for writing or bail early if it cannot be
-	# obtained.
-	raw_conn = connection.getConnection()
-	raw_conn.execute('BEGIN IMMEDIATE TRANSACTION')
+        # try to initiate / create the database
+        database = output_dir + "/HappyFace.db"
+        try:
+            # has to be the full path
+            connection_string = 'sqlite:' + os.getcwd() + "/" + database
+            connection = connectionForURI(connection_string)
+            sqlhub.processConnection = connection
+        except Exception, ex:
+            raise Exception('Could not initiate or create the database ' + os.getcwd() + "/" + database + ': ' + str(ex))
 
-        sqlhub.processConnection = connection
-    except Exception, ex:
-        sys.stdout.write('Could not initiate or create the database ' + os.getcwd() + "/" + database + ': ' + str(ex) + '\nAborting ...\n')
-        sys.exit(-1)
+        # definition of the global timeout
+        timeout        =  int(config.get('setup','timeout_module'))
+        timeoutDownload =  int(config.get('setup','timeout_download'))
 
-    # definition of the global timeout
-    timeout        =  int(config.get('setup','timeout_module'))
-    timeoutDownload =  int(config.get('setup','timeout_download'))
+        # definition of the holdback_time
+        holdback_time  =  int(config.get('setup','holdback_time'))
 
-    # definition of the holdback_time
-    holdback_time  =  int(config.get('setup','holdback_time'))
+        ##########################################################
+        ################ start with the execution ################
 
-    ##########################################################
-    ################ start with the execution ################
-
-    # create options dictionary
-    module_options = {}
+        # create options dictionary
+        module_options = {}
     
-    module_options["timestamp"] = timestamp
-    module_options["archive_dir"] = archive_dir
-    module_options["holdback_time"] = holdback_time
+        module_options["timestamp"] = timestamp
+        module_options["archive_dir"] = archive_dir
+        module_options["holdback_time"] = holdback_time
     
-    # dictionary collects the module objects
-    modObj_list = {}
+        # dictionary collects the module objects
+        modObj_list = {}
 
-    # initialisation of modules
-    print "HappyFace: Start with module preparation:"
-    moduleCount = 0
-    for category in config.get('setup','categories').split(","):
-	for module in config.get(category,'modules').split(","):
+        # initialisation of modules
+        print "HappyFace: Start with module preparation:"
+        moduleCount = 0
+        for category in config.get('setup','categories').split(","):
+	    for module in config.get(category,'modules').split(","):
 
-	    if module == "": continue
+	        if module == "": continue
 
-            # import module class dynamically
-	    modClass = __import__(module)
-            moduleCount += 1
+                # import module class dynamically
+                modClass = __import__(module)
+                moduleCount += 1
 	    
-	    module_options["category"] = category
+                module_options["category"] = category
 
-       	    # create a object of the class dynamically
-       	    modObj_list[module] = getattr(modClass,module)(module_options)
-            print "  "+str(moduleCount)+": "+module
+                # create a object of the class dynamically
+                modObj_list[module] = getattr(modClass,module)(module_options)
+                print "  "+str(moduleCount)+": "+module
 
-    print "HappyFace: Module preparation finished."
+        print "HappyFace: Module preparation finished."
 
-    # Preparation of Download and CSS Service.
-    # Selecting all files for download.
-    # Prepare list of needed css files for the webpage.
-    downloadService = DownloadService(tmp_dir)
-    cssService = CssService(output_dir,'config/modules')
+        # Preparation of Download and CSS Service.
+        # Selecting all files for download.
+        # Prepare list of needed css files for the webpage.
+        downloadService = DownloadService(tmp_dir)
+        cssService = CssService(output_dir,'config/modules')
     
-    for module in modObj_list.keys():
-        for downloadTag in modObj_list[module].getDownloadRequests():
-            downloadService.add(downloadTag)
+        for module in modObj_list.keys():
+            for downloadTag in modObj_list[module].getDownloadRequests():
+                downloadService.add(downloadTag)
             
-        cssService.add(modObj_list[module].getCssRequests())
+            cssService.add(modObj_list[module].getCssRequests())
 
-    # Start parallel download of all specified files
-    downloadService.download(timeoutDownload)
+        # Start parallel download of all specified files
+        downloadService.download(timeoutDownload)
 
-    # parallel execution of the modules (threading)
-    # see therefore: http://www.wellho.net/solutions/python-python-threads-a-first-example.html
-    for module in modObj_list.keys():
-        # make downloadService available for each module
-        modObj_list[module].setDownloadService(downloadService)
-        # execute the object in a thread
-        modObj_list[module].start()
+        # parallel execution of the modules (threading)
+        # see therefore: http://www.wellho.net/solutions/python-python-threads-a-first-example.html
+        for module in modObj_list.keys():
+            # make downloadService available for each module
+            modObj_list[module].setDownloadService(downloadService)
+            # execute the object in a thread
+            modObj_list[module].start()
 
-    # Release the database lock before module processing, otherwise the modules
-    # cannot write to the database since the threads they run in acquire a new
-    # connection which cannot get the lock we are holding.
-    raw_conn.execute('END');
+        print "HappyFace: Start module processing." 
 
-    print "HappyFace: Start module processing." 
-
-    # wait till all modules are finished OR till the global timeout
-    for module in modObj_list.keys():
-        start = int(time())
-        modObj_list[module].join(timeout)
-        timeout -= int(time()) - start
-        if timeout < 1:
-            break
+        # wait till all modules are finished OR till the global timeout
+        for module in modObj_list.keys():
+            start = int(time())
+            modObj_list[module].join(timeout)
+            timeout -= int(time()) - start
+            if timeout < 1:
+                break
 
 
-    for module in modObj_list.keys():
-        if modObj_list[module].isAlive() == True:
-            modObj_list[module]._Thread__stop()
-            modObj_list[module].error_message += "\nCould not execute module in time, "\
-                                                 + modObj_list[module].__module__ \
-                                                 + " aborting ...\n"
-            sys.stdout.write(modObj_list[module].error_message)
+        for module in modObj_list.keys():
+            if modObj_list[module].isAlive() == True:
+                modObj_list[module]._Thread__stop()
+                modObj_list[module].error_message += "\nCould not execute module in time, "\
+                                                     + modObj_list[module].__module__ \
+                                                     + " aborting ...\n"
+                sys.stdout.write(modObj_list[module].error_message)
 
-        # store results (or pre-defined values if timeout) to DB
-	# if enabled, erase old data from the DB
-        modObj_list[module].processDB()
+            # store results (or pre-defined values if timeout) to DB
+	    # if enabled, erase old data from the DB
+            modObj_list[module].processDB()
 
-    print "HappyFace: Module processing finished." 
+        print "HappyFace: Module processing finished." 
 
-    # create final PHP/HTML output
+        # create final PHP/HTML output
 
-    print "HappyFace: Start output processing:" 
+        print "HappyFace: Start output processing:" 
 
-    for type in config.get('setup','output_types').split(","):
-	if type == "web":
-	     print "HappyFace: Start building web page:" 
+        for type in config.get('setup','output_types').split(","):
+            if type == "web":
+	        print "HappyFace: Start building web page:" 
 
-	     theFinalWebOutput = WebCreator(config,modObj_list,timestamp)
-	     theFinalWebOutput.setCss(cssService.getCssWebDirFiles())
-	     webpage_output = theFinalWebOutput.getOutput()
+                theFinalWebOutput = WebCreator(config,modObj_list,timestamp)
+                theFinalWebOutput.setCss(cssService.getCssWebDirFiles())
+                webpage_output = theFinalWebOutput.getOutput()
 
-	     # sync the module css files to the wepage directory
-	     cssService.syncCssFiles()
+                # sync the module css files to the wepage directory
+                cssService.syncCssFiles()
     
-	     # save webpage output file
-             try:
-                  f = open(output_dir + '/index.php','w')
-                  f.write(webpage_output)
-                  f.close()
-             except:
-                  sys.stdout.write('Could not create final output ' + output_dir + '/index.php , aborting ...\n')
-                  sys.exit(-1)
+                # save webpage output file
+                try:
+                    f = open(output_dir + '/index.php','w')
+                    f.write(webpage_output)
+                    f.close()
+                except Exception, ex:
+                    raise Exception('Could not create final output ' + output_dir + '/index.php:' + str(ex))
 
-             print "HappyFace: Web page building finished." 
+                print "HappyFace: Web page building finished." 
 
-    print "HappyFace: Output processing finished." 
+        print "HappyFace: Output processing finished." 
 
-    # cleanup
-    downloadService.clean()
+        # cleanup
+        downloadService.clean()
 
-    print "\nDONE!!\n"
+        print "\nDONE!!\n"
+    except Exception, ex:
+        print str(ex) + '\nAborting ...'
+    finally:
+        try:
+            os.unlink(lockfile)
+	except: 
+	    pass
 
 if __name__ == '__main__':
     HappyFace()
