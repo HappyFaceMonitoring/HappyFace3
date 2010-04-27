f6735da47796be541b67e3b6bd4b14554de019de
Armin Scheurer <contact@armin-scheurer.de>
2010-04-27 14:27:09 +0000

    Added CMS dashboard plot about Grid job activity at GridKa, sorted by user
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/trunk@426 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

---
+++ HappyFace/modules/phpplotdashboard_job_summary_user.py
@@ -0,0 +1,8 @@
+from PhpPlotDashboardJobSummary import *
+import time
+
+class phpplotdashboard_job_summary_user(PhpPlotDashboardJobSummary):
+
+    def __init__(self,module_options):
+
+        PhpPlotDashboardJobSummary.__init__(self,module_options)
