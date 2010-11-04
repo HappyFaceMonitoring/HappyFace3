5e7e32590eba984cab6708bd2ba405fa5229964d
Armin Scheurer <contact@armin-scheurer.de>
2010-11-04 09:37:39 +0000

    Removed old lxarda-dashboard module. It is deprecated now and replaced by the cms-dashboard module
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/trunk@555 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

--- HappyFace/modules/phpplotdashboard_jobs_succ_fail.py
+++ HappyFace/modules/phpplotdashboard_jobs_succ_fail.py
@@ -5,3 +5,6 @@ class phpplotdashboard_jobs_succ_fail(PhpPlotDashboard):
     def __init__(self,module_options):
 
         PhpPlotDashboard.__init__(self,module_options)
+
+	# Also load and show the pie chart
+        self.downloadRequest['plot2'] = 'wget|'+self.makeUrl({'type': 'nsfp'})
