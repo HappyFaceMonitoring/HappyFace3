a4cbb24ccd0610f359f231be56b507bad63f873e
Stefan Wayand <stefan.wayand@cern.ch>
2012-04-25 07:18:48 +0000

    adding module for tape monitoring
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/trunk@737 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

---
+++ HappyFace/modules/cern_dcachetape_info.py
@@ -0,0 +1,6 @@
+from  CERNdCacheTapeinfo import *
+class  cern_dcachetape_info(CERNdCacheTapeinfo):
+
+    def __init__(self,module_options):
+
+        CERNdCacheTapeinfo.__init__(self,module_options)
