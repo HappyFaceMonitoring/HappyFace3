ee754e11eec4ad0d18491faf4ac5a19380e41135
Gregor Vollmer <vollmer@ekp.uni-karlsruhe.de>
2012-07-24 15:54:42 +0000

    Optional environment loading and 'manual' for HF tools
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/branches/v3.0@754 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

--- hf/tools/shell.py
+++ hf/tools/shell.py
@@ -5,5 +5,7 @@
 import code, traceback, sys
 import hf
 
+load_hf_environment = True
+
 def execute():
     code.interact("Interactive HappyFace Shell", local={'hf':hf})
--- tools.py
+++ tools.py
@@ -8,48 +8,52 @@ import logging
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
+    if len(sys.argv) < 2:
         print "Usage: %s TOOL [options]\nUse the 'help'-tool for more information\n" % sys.argv[0]
         sys.exit(-1)
+    
+    tool_name = sys.argv[1]
+    sys.argv[0] += ' ' + sys.argv[1]
+    del sys.argv[1]
+
+    import hf.tools
+    try:
+        tool = __import__("hf.tools."+tool_name, fromlist=[hf.tools], globals=globals())
+    except ImportError,e:
+        logger.error("No tool called %s: %s" % (tool_name, str(e)))
+        sys.exit(-1)
+    
     try:
         hf.hf_dir = os.path.dirname(os.path.abspath(__file__))
-        hf.configtools.readConfigurationAndEnv()
-        hf.configtools.setupLogging('acquire_logging_cfg')
-    except Exception,e:
-        print "Setting up HappyFace failed"
-        traceback.print_exc()
-        sys.exit(-1)
-    try:
-        
-        cfg_dir = None
-        try:
-            hf.configtools.importModules()
-            
-            hf.database.connect(implicit_execution = True)
-            hf.database.metadata.create_all()
-            
-            category_list = hf.category.createCategoryObjects()
-        except Exception, e:
-            logger.error("Setting up HappyFace failed: %s", str(e))
-            logger.debug(traceback.format_exc())
-        
-        tool_name = sys.argv[1]
-        del sys.argv[1]
-        
-        import hf.tools
-        try:
-            tool = __import__("hf.tools."+tool_name, fromlist=[hf.tools], globals=globals())
-        except ImportError,e:
-            logger.error("No tool called %s: %s" % (tool_name, str(e)))
-            sys.exit(-1)
-        
+        if tool.load_hf_environment:
+            try:
+                hf.configtools.readConfigurationAndEnv()
+                hf.configtools.setupLogging('acquire_logging_cfg')
+            except Exception,e:
+                print "Setting up HappyFace failed"
+                traceback.print_exc()
+                sys.exit(-1)
+
+            cfg_dir = None
+            try:
+                hf.configtools.importModules()
+                
+                hf.database.connect(implicit_execution = True)
+                hf.database.metadata.create_all()
+                
+                category_list = hf.category.createCategoryObjects()
+            except Exception, e:
+                print "Setting up HappyFace failed: %s", str(e)
+                print traceback.format_exc()
         try:
             tool.execute()
         except Exception, e:
-            logger.error("HappyFace Tool execution failed: %s", str(e))
+            print "Tool execution failed: %s", str(e)
+            print traceback.format_exc()
         
     except Exception, e:
         logger.error("Uncaught HappyFace exception: %s", str(e))
         logger.debug(traceback.format_exc())
     finally:
-        hf.database.disconnect()
+        if tool.load_hf_environment:
+            hf.database.disconnect()
