c192c27b385bbb7f982d14f2c3763fd38a4f10c0
Gregor Vollmer <vollmer@ekp.uni-karlsruhe.de>
2012-08-09 08:36:29 +0000

    Started common constraint logic in plot generator
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/branches/v3.0@799 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

--- static/js/timeseries.js
+++ static/js/timeseries.js
@@ -1,4 +1,26 @@
 $(function() {
+ 
+var common_constraints_enabled = true;
+var curve_num = 0;
+
+function check_common_constraint_condition()
+{
+    common_constraints_enabled = true;
+    module = $("#curve_1_module_instance option:selected").val();
+    subtable = $("#curve_1_subtable option:selected").val();
+    for(var i=2; i<= curve_num; i += 1) {
+        if($("#curve_"+i+"_module_instance option:selected").val() != module
+            || $("#curve_"+i+"_subtable option:selected").val() != subtable) {
+            common_constraints_enabled = false;
+            break;
+        }
+    }
+            
+    if(common_constraints_enabled)
+        $('#common_constraints_info').fadeOut(250);
+    else
+        $('#common_constraints_info').fadeIn(250);
+}
 
 function changed_subtable(e)
 {
@@ -15,6 +37,7 @@ function changed_subtable(e)
         html += modules[module][subtable][idx] + "</option>";
     }
     $("#"+curve+"_variable").empty().append(html);
+    check_common_constraint_condition();
 }
 
 function changed_module(e)
@@ -28,9 +51,9 @@ function changed_module(e)
     }
     $("#"+curve+"_subtable").empty().append(html);
     changed_subtable(e);
+    check_common_constraint_condition();
 }
 
-var curve_num = 0;
 function add_curve(initial_mod, initial_table, initial_variable, initial_title) {
     curve_num += 1;
     html  = "<fieldset class=\"floating\"><legend>Curve "+curve_num+"</legend>";
@@ -69,6 +92,55 @@ function add_curve(initial_mod, initial_table, initial_variable, initial_title) 
     html += " </select></label></p>";
     html += " <p><label><span>Title</span><input id=\"curve_"+curve_num+"_title\" type=\"edit\" name=\"curve_"+curve_num+"_title\" value=\"";
     html += initial_title+"\"></label></p>";
+    html += "<!--<p class='button_box floating'><a class='small add' href='#'>+</a><a class='small remove' href='#'>-</a></p>"
+    html += "<h3>Constraints</h3>-->";
+    html += "</fieldset>";
+    $("#curve_controls").before(html);
+    $("#curve_"+curve_num+"_module_instance").change({'curve': "curve_"+curve_num}, changed_module);
+    $("#curve_"+curve_num+"_subtable").change({'curve': "curve_"+curve_num}, changed_subtable);
+    
+    return false;
+}
+
+function get_constraint_html(variable, data) {
+    curve_num += 1;
+    html  = "<fieldset class=\"floating\"><legend>Curve "+curve_num+"</legend>";
+    html += " <p><label><span>Module</span><select id=\"curve_"+curve_num+"_module_instance\" name=\"curve_"+curve_num+"_module_instance\">";
+    var first_mod = initial_mod;
+    for(var mod in modules) {
+        if(first_mod == "") first_mod = mod;
+        if(first_mod == mod) html += " <option selected=\"selected\"";
+        else html += " <option";
+        html += " value=\"" + mod + "\">" + mod + " (" + module_types[mod] + ")</option>";
+    }
+    html += " </select></label></p>";
+    html += " <p><label><span>Subtable</span><select id=\"curve_"+curve_num+"_subtable\" name=\"curve_"+curve_num+"_subtable\">";
+    var found_subtable = false;
+    for(var subtable in modules[first_mod]) {
+        if(subtable == initial_table) {
+            html += " <option selected=\"selected\">";
+            found_subtable = true;
+        }
+        else
+            html += " <option>";
+        html += subtable + "</option>";
+    }
+    if(!found_subtable)
+        initial_table = '';
+    
+    html += " </select></label></p>";
+    html += " <p><label><span>Variable</span><select  id=\"curve_"+curve_num+"_variable\" name=\"curve_"+curve_num+"_variable\">";
+    for(var idx in modules[first_mod][initial_table]) {
+        if(modules[first_mod][initial_table][idx] == initial_variable)
+            html += " <option selected=\"selected\">";
+        else
+            html += " <option>";
+        html += modules[first_mod][initial_table][idx] + "</option>";
+    }
+    html += " </select></label></p>";
+    html += " <p><label><span>Title</span><input id=\"curve_"+curve_num+"_title\" type=\"edit\" name=\"curve_"+curve_num+"_title\" value=\"";
+    html += initial_title+"\"></label></p>";
+    
     html += "</fieldset>";
     $("#curve_controls").before(html);
     $("#curve_"+curve_num+"_module_instance").change({'curve': "curve_"+curve_num}, changed_module);
