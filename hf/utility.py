f27be78eae8763f09120475caa826a1cb24bab50
Gregor Vollmer <vollmer@ekp.uni-karlsruhe.de>
2012-10-29 07:37:36 +0000

    Added matheval() function w/ documentation
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFace/branches/v3.0@874 5ec1a27d-59bc-451c-a1e5-77859e3e35f3

--- hf/utility.py
+++ hf/utility.py
@@ -14,6 +14,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import logging, traceback, math
+
 def prettyDataSize(size_in_bytes):
     """ Takes a data size in bytes and formats a pretty string. """
     unit = "B"
@@ -32,3 +34,49 @@ def prettyDataSize(size_in_bytes):
         unit = "TiB"
     print size_in_bytes, "%.1f "%size_in_bytes + unit
     return "%.1f "%size_in_bytes + unit
+
+matheval_context = {
+    "abs": abs,
+    "sin": math.sin,
+    "cos": math.cos,
+    "sqrt": math.sqrt,
+    "pow": math.pow,
+    "floor": math.floor,
+    "ceil": math.ceil,
+    "max": max,
+    "min": min,
+    "float": float,
+    "int": int,
+}
+def matheval(input, variables={}):
+    """
+    A safe way to evaluate mathematical expressions.
+    
+    The following Python functions are available
+    
+     * abs
+     * sin
+     * cos
+     * sqrt
+     * pow
+     * floor
+     * ceil
+     * max
+     * min
+     * float
+     * int
+    
+    :param input: String with mathematical expression
+    :param variables: A dictionary with variables available for  input expression
+    :returns: The result of the expression, or *None* in case of error.
+    """
+    
+    # block everything, even builtins, and add "safe" mathematical functions.
+    # Statements won't work, as well as potentially dangerous things like
+    # __import__ or open.
+    variables.update(matheval_context)
+    try:
+        return eval(input, {"__builtins__": None}, variables)
+    except Exception, e:
+        logging.error(traceback.format_exc())
+        return None
